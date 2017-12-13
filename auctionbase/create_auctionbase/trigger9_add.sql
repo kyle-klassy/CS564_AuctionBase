d9c034ffed1d297d8d2df406c07c8516d1d5f659
cubanboo <cubanboo17@gmail.com>
2017-12-13 11:30:38 -0600

    trigger9 is functional: buyPrice has been reached

--- auctionbase/create_auctionbase/trigger9_add.sql
+++ auctionbase/create_auctionbase/trigger9_add.sql
@@ -1,12 +1,12 @@
--- description: Constraint 11
+-- description: Constraint 12
 
 PRAGMA foreign_keys = ON;
 
-drop trigger if exists trigger6;
+drop trigger if exists trigger9;
 
 create trigger trigger9
     before insert on Bids
-    for each row when (NEW.Amount > (Select i.Buy_Price from Items i where NEW.ItemID = i.ItemID))
+    for each row when (Select i.Currently from Items i where NEW.ItemID = i.ItemID and i.Currently >= i.Buy_Price) 
     begin
         SELECT raise(rollback, ‘Trigger9_Failed’);
     end;
